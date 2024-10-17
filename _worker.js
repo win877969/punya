a89de93d619ceeacb1eea9aeaaeb414d0c6259b5
win877969 <xpandhita@gmail.com>
2024-10-17 13:56:05 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -709,7 +709,7 @@ function getวเลสConfig(userIDs, hostName) {
 	// Prepare output string for each userID
 	const output = userIDArray.map((userID) => {
 		const วเลสMain = atob(pt) + '://' + userID + atob(at) + hostName + commonUrlPart;
-		const วเลสSec = atob(pt) + '://' + userID + atob(at) + พร็อกซีไอพี + commonUrlPart1;
+		const วเลสSec = atob(pt) + '://' + userID + atob(at) + hostName + commonUrlPart1;
 		return `<h2>VLESS PORT 433</h2>
 ---------------------------------------------------------------
 ${วเลสMain}

7bd70a3b409dcd5aad0e9c9d8ea13b349cfd0427
win877969 <xpandhita@gmail.com>
2024-10-17 12:12:22 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -31,7 +31,7 @@ export default {
 		// uuid_validator(request);
 		try {
 			userID = env.UUID || userID;
-			Proxy IP = env.Proxy IP || Proxy IP;
+			ProxyIP = env.ProxyIP || ProxyIP;
 			dohURL = env.DNS_RESOLVER_URL || dohURL;
 			let userID_Path = userID;
 			if (userID.includes(',')) {
